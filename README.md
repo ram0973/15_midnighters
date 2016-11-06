dd18f4d4d7a94b85476b81b3a33f1cc580779943
ram0973 <ram0973@gmail.com>
2016-11-06 15:55:57 +0300

    начальный коммит

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# 15_midnighters
+# Решение задачи [№15](https://devman.org/challenges/15/) с сайта [devman.org](https://devman.org)
+
+## Условие задачи:
+
+В базе devman хранится информация о том кто и когда отправил задачу 
+на проверку. 
+Получить её можно с помощью [АПИ](http://devman.org/api/challenges/solution_attempts/?page=2)
+
+Давай выясним кто отправлял задачи на проверку после 24:00.
+
+Рекомендуемые библиотеки: requests и pytz.
+
+## Системные требования
+
+```
+Python 3.5.2+
+win-unicode-console
+requests
+pendulum
+```
+
+## Установка
+
+Windows
+
+```    
+git clone https://github.com/ram0973/15_midnighters.git
+cd 15_midnighters
+(Windows) pip install -r requirements.txt
+(Linux) pip3 install -r requirements.txt
+```
+    
+## Описание работы
+
+```
+Скрипт выводит список всех пользователей, которые сдавали задания с 
+0 до 05:59:59 часов 
+```
+
+## Запуск
+
+```
+(Windows) python seek_dev_midnighters.py
+(Linux) python3 seek_dev_midnighters.py
+```
+ 
+## Лицензия
+
+[MIT](http://opensource.org/licenses/MIT)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,46 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+# -*- coding: utf-8 -*-
+import requests
+from datetime import datetime
+import pendulum
+from helpers import enable_win_unicode_console, handle_requests_exceptions
 
-def get_midnighters():
-    pass
+DEVMAN_ATTEMPTS_URL = 'https://devman.org/api/challenges/solution_attempts/'
+MIDNIGHT_END = 6
+
+
+@handle_requests_exceptions
+def load_attempts():
+    """ Получаем json с данными по попыткам сдачи """
+    response = requests.get(DEVMAN_ATTEMPTS_URL)
+    response.raise_for_status()
+    for page in range(1, response.json()['number_of_pages']+1):
+        page_items = requests.get(
+            DEVMAN_ATTEMPTS_URL, params={'page': page}).json()['records']
+        yield from page_items
+
+
+def is_midnight(attempt: dict) -> bool:
+    """ Проверяем, полуночная ли была попытка
+    :param attempt: словарь с данными попытки сдачи
+    :return: True если попытка была полуночной
+    """
+    if attempt['timezone'] is None or attempt['timestamp'] is None:
+        return None
+    attempt_time = pendulum.timezone(attempt['timezone']).convert(
+        datetime.fromtimestamp(attempt['timestamp']))
+    return True if attempt_time.hour < MIDNIGHT_END else False
+
+
+def get_midnighters() -> list:
+    return sorted({attempt['username'] for attempt in load_attempts()
+                   if is_midnight(attempt)})
+
+
+def main():
+    enable_win_unicode_console()
+    print('\nСписок кодеров - полуночников:')
+    print(', '.join(get_midnighters()))
+
 
 if __name__ == '__main__':
-  pass
+    main()
